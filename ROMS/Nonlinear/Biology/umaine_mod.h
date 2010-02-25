5c7c7ff4e21b8baa8c57974774a9c4f2175da15c
kate <kate@f091316a-d328-0410-a40a-876eff57d070>
2010-02-25 07:07:17 +0000

    Rest of ticket 388 patch.
    git-svn-id: https://www.myroms.org/svn/omlab/branches/kate/trunk@1166 f091316a-d328-0410-a40a-876eff57d070

--- ROMS/Nonlinear/Biology/umaine_mod.h
+++ ROMS/Nonlinear/Biology/umaine_mod.h
@@ -66,54 +66,148 @@
 !   ro7      Grazing preference for detritus [nondimensional].         !
 !=======================================================================
 !
-        USE mod_param
+      USE mod_param
 
-        implicit none
-        
-        integer, dimension(Ngrids) :: BioIter
+      implicit none
+!
+!  Set biological tracer identification indices.
+!
+      integer, pointer :: idbio(:)    ! Biological tracers
+      integer :: iNO3_                ! Nitrate concentration
+      integer :: iNH4_                ! Ammonium concentration
+      integer :: iSiOH                ! Silicate concentration
+      integer :: iSphy                ! Samll phytoplankton
+      integer :: iLphy                ! Diatom concentration
+      integer :: iSzoo                ! Small zooplankotn concentration
+      integer :: iLzoo                ! Mesozooplankotn concentration
+      integer :: iSDet                ! Detritus notrogen concentration
+      integer :: iopal                ! Biogenic silicate concentration
+      integer :: iPO4_                ! Phosphate concentration
+#ifdef OXYGEN
+      integer :: iOxyg                ! Dissolved oxygen concentration
+#endif
+#ifdef CARBON
+      integer :: iTIC_                ! Total inorganic carbon
+      integer :: iTAlk                ! Total alkalinity
+#endif
 
-        real(r8), dimension(Ngrids) :: reg1          ! 1/day
-        real(r8), dimension(Ngrids) :: reg2          ! 1/day
-        real(r8), dimension(Ngrids) :: gmaxs1        ! 1/day
-        real(r8), dimension(Ngrids) :: gmaxs2        ! 1/day
-        real(r8), dimension(Ngrids) :: beta1         ! 1/day
-        real(r8), dimension(Ngrids) :: beta2         ! 1/day
-        real(r8), dimension(Ngrids) :: akz1          ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: akz2          ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: PARfrac       ! nondimensional
-        real(r8), dimension(Ngrids) :: amaxs2        ! 1/(Watts/m2)/day
-        real(r8), dimension(Ngrids) :: pis1          ! m3/mmol_N
-        real(r8), dimension(Ngrids) :: pis2          ! m3/mmol_N
-        real(r8), dimension(Ngrids) :: akno3s1       ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: aknh4s1       ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: akpo4s1       ! mmol_P/m3
-        real(r8), dimension(Ngrids) :: akco2s1       ! mmol_C/m3
-        real(r8), dimension(Ngrids) :: akno3s2       ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: aknh4s2       ! mmol_N/m3
-        real(r8), dimension(Ngrids) :: aksio4s2      ! mmol_Si/m3
-        real(r8), dimension(Ngrids) :: akpo4s2       ! mmol_P/m3
-        real(r8), dimension(Ngrids) :: akco2s2       ! mmol_C/m3
-        real(r8), dimension(Ngrids) :: ak1           ! 1/m
-        real(r8), dimension(Ngrids) :: ak2           ! 1/m/(mmol_N/m3)
-        real(r8), dimension(Ngrids) :: parsats1      ! Watts/m2
-        real(r8), dimension(Ngrids) :: parsats2      ! Watts/m2
-        real(r8), dimension(Ngrids) :: bgamma0       ! 1/day
-        real(r8), dimension(Ngrids) :: bgamma1       ! [nondimensional]
-        real(r8), dimension(Ngrids) :: bgamma2       ! [nondimensional]
-        real(r8), dimension(Ngrids) :: bgamma3       ! 1/day
-        real(r8), dimension(Ngrids) :: bgamma4       ! 1/day
-        real(r8), dimension(Ngrids) :: bgamma5       ! 1/day
-        real(r8), dimension(Ngrids) :: bgamma6       ! 
-        real(r8), dimension(Ngrids) :: bgamma7       ! 1/day
-        real(r8), dimension(Ngrids) :: wsd           ! m/day
-        real(r8), dimension(Ngrids) :: wsdsi         ! m/day
-        real(r8), dimension(Ngrids) :: wsp           ! m/day
-        real(r8), dimension(Ngrids) :: si2n          ! mol_Si/mol_N
-        real(r8), dimension(Ngrids) :: pco2a         ! ppmv
-        real(r8), dimension(Ngrids) :: p2n           ! mol_P/mol_N
-        real(r8), dimension(Ngrids) :: o2no          ! mol_O2/mol_NO3
-        real(r8), dimension(Ngrids) :: o2nh          ! mol_O2/mol_NH4
-        real(r8), dimension(Ngrids) :: c2n           ! mol_C/mol_N
-        real(r8), dimension(Ngrids) :: ro5           ! nondimensional
-        real(r8), dimension(Ngrids) :: ro6           ! nondimensional
-        real(r8), dimension(Ngrids) :: ro7           ! nondimensional
+      integer, dimension(Ngrids) :: BioIter
+
+      real(r8), dimension(Ngrids) :: reg1            ! 1/day
+      real(r8), dimension(Ngrids) :: reg2            ! 1/day
+      real(r8), dimension(Ngrids) :: gmaxs1          ! 1/day
+      real(r8), dimension(Ngrids) :: gmaxs2          ! 1/day
+      real(r8), dimension(Ngrids) :: beta1           ! 1/day
+      real(r8), dimension(Ngrids) :: beta2           ! 1/day
+      real(r8), dimension(Ngrids) :: akz1            ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: akz2            ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: PARfrac         ! nondimensional
+      real(r8), dimension(Ngrids) :: amaxs2          ! 1/(Watts/m2)/day
+      real(r8), dimension(Ngrids) :: pis1            ! m3/mmol_N
+      real(r8), dimension(Ngrids) :: pis2            ! m3/mmol_N
+      real(r8), dimension(Ngrids) :: akno3s1         ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: aknh4s1         ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: akpo4s1         ! mmol_P/m3
+      real(r8), dimension(Ngrids) :: akco2s1         ! mmol_C/m3
+      real(r8), dimension(Ngrids) :: akno3s2         ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: aknh4s2         ! mmol_N/m3
+      real(r8), dimension(Ngrids) :: aksio4s2        ! mmol_Si/m3
+      real(r8), dimension(Ngrids) :: akpo4s2         ! mmol_P/m3
+      real(r8), dimension(Ngrids) :: akco2s2         ! mmol_C/m3
+      real(r8), dimension(Ngrids) :: ak1             ! 1/m
+      real(r8), dimension(Ngrids) :: ak2             ! 1/m/(mmol_N/m3)
+      real(r8), dimension(Ngrids) :: parsats1        ! Watts/m2
+      real(r8), dimension(Ngrids) :: parsats2        ! Watts/m2
+      real(r8), dimension(Ngrids) :: bgamma0         ! 1/day
+      real(r8), dimension(Ngrids) :: bgamma1         ! [nondimensional]
+      real(r8), dimension(Ngrids) :: bgamma2         ! [nondimensional]
+      real(r8), dimension(Ngrids) :: bgamma3         ! 1/day
+      real(r8), dimension(Ngrids) :: bgamma4         ! 1/day
+      real(r8), dimension(Ngrids) :: bgamma5         ! 1/day
+      real(r8), dimension(Ngrids) :: bgamma6         ! 
+      real(r8), dimension(Ngrids) :: bgamma7         ! 1/day
+      real(r8), dimension(Ngrids) :: wsd             ! m/day
+      real(r8), dimension(Ngrids) :: wsdsi           ! m/day
+      real(r8), dimension(Ngrids) :: wsp             ! m/day
+      real(r8), dimension(Ngrids) :: si2n            ! mol_Si/mol_N
+      real(r8), dimension(Ngrids) :: pco2a           ! ppmv
+      real(r8), dimension(Ngrids) :: p2n             ! mol_P/mol_N
+      real(r8), dimension(Ngrids) :: o2no            ! mol_O2/mol_NO3
+      real(r8), dimension(Ngrids) :: o2nh            ! mol_O2/mol_NH4
+      real(r8), dimension(Ngrids) :: c2n             ! mol_C/mol_N
+      real(r8), dimension(Ngrids) :: ro5             ! nondimensional
+      real(r8), dimension(Ngrids) :: ro6             ! nondimensional
+      real(r8), dimension(Ngrids) :: ro7             ! nondimensional
+      real(r8), dimension(Ngrids) :: pCO2air         ! ppmv
+
+      CONTAINS
+
+      SUBROUTINE initialize_biology
+!
+!=======================================================================
+!                                                                      !
+!  This routine sets several variables needed by the biology model.    !
+!  It allocates and assigns biological tracers indices.                !
+!                                                                      !
+!=======================================================================
+!
+!  Local variable declarations
+!
+      integer :: i, ic
+!
+!-----------------------------------------------------------------------
+!  Determine number of biological tracers.
+!-----------------------------------------------------------------------
+!
+#ifdef CARBON
+# ifdef OXYGEN
+      NBT=13
+# else
+      NBT=12
+# endif
+#else
+# ifdef OXYGEN
+      NBT=11
+# else
+      NBT=10
+# endif
+#endif
+!
+!-----------------------------------------------------------------------
+!  Initialize tracer identification indices.
+!-----------------------------------------------------------------------
+!
+!  Allocate biological tracer vector.
+!
+      IF (.not.allocated(idbio)) THEN
+        allocate ( idbio(NBT) )
+      END IF
+!
+!  Set identification indices.
+!
+      ic=NAT+NPT+NCS+NNS
+      DO i=1,NBT
+        idbio(i)=ic+i
+      END DO
+      iNO3_=ic+1
+      iNH4_=ic+2
+      iChlo=ic+3
+      iPhyt=ic+4
+      iZoop=ic+5
+      iLDeN=ic+6
+      iSDeN=ic+7
+      ic=ic+7
+# ifdef CARBON
+      iLDeC=ic+1
+      iSDeC=ic+2
+      iTIC_=ic+3
+      iTAlk=ic+4
+      ic=ic+4
+# endif
+# ifdef OXYGEN
+      iOxyg=ic+1
+      ic=ic+1
+# endif
+
+      RETURN
+      END SUBROUTINE initialize_biology
