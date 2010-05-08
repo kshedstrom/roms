3c415723b1808f5bab75439e0ee555794753ea32
kate <kate@f091316a-d328-0410-a40a-876eff57d070>
2010-05-08 01:03:29 +0000

    Tiny thing.
    git-svn-id: https://www.myroms.org/svn/omlab/branches/kate/trunk@1205 f091316a-d328-0410-a40a-876eff57d070

--- Compilers/Darwin-gfortran.mk
+++ Compilers/Darwin-gfortran.mk
@@ -67,7 +67,7 @@ else
 endif
              LIBS += -L$(NETCDF_LIBDIR) -lnetcdf
 ifdef USE_NETCDF4
-             LIBS += -L$(HDF5_LIBDIR) -lhdf5_hl -lhdf5 -lz
+             LIBS += -L$(HDF5_LIBDIR) -lhdf5_hl -lhdf5 -lz -lcurl
 endif
 
 ifdef USE_ARPACK
