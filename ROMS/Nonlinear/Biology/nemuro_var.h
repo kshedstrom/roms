8e2bf11becfb9f3f76c20861759d6e094c39362f
Kate Hedstrom <kate@arsc.edu>
2010-10-14 14:07:43 -0800

    Offline floats now working for CCS30.

--- ROMS/Nonlinear/Biology/nemuro_var.h
+++ ROMS/Nonlinear/Biology/nemuro_var.h
@@ -57,9 +57,41 @@
                 idOPALsed=varid
 # endif
 
-/*
-**  Biological tracers open boundary conditions.
-*/
+! All over again for climatatology
+              CASE ('idTclm(iLphy)')
+                idTclm(iLphy)=varid
+              CASE ('idTclm(iSphy)')
+                idTclm(iSphy)=varid
+              CASE ('idTclm(iLzoo)')
+                idTclm(iLzoo)=varid
+              CASE ('idTclm(iSzoo)')
+                idTclm(iSzoo)=varid
+              CASE ('idTclm(iPzoo)')
+                idTclm(iPzoo)=varid
+              CASE ('idTclm(iNO3_)')
+                idTclm(iNO3_)=varid
+              CASE ('idTclm(iNH4_)')
+                idTclm(iNH4_)=varid
+              CASE ('idTclm(iPON_)')
+                idTclm(iPON_)=varid
+              CASE ('idTclm(iDON_)')
+                idTclm(iDON_)=varid
+              CASE ('idTclm(iSiOH)')
+                idTclm(iSiOH)=varid
+              CASE ('idTclm(iopal)')
+                idTclm(iopal)=varid
+# ifdef IRON_LIMIT
+              CASE ('idTclm(iFeSp)')
+                idTclm(iFeSp)=varid
+              CASE ('idTclm(iFeLp)')
+                idTclm(iFeLp)=varid
+              CASE ('idTclm(iFeD_)')
+                idTclm(iFeD_)=varid
+# endif
+
+!
+!  Biological tracers open boundary conditions.
+!
 
               CASE ('idTbry(iwest,iLphy)')
                 idTbry(iwest,iLphy)=varid
@@ -188,9 +220,9 @@
 
 #ifdef TS_PSOURCE
 
-/*
-**  Biological tracers point Source/Sinks (river runoff).
-*/
+!
+! Biological tracers point Source/Sinks (river runoff).
+!
 
               CASE ('idRtrc(iNO3_)')
                 idRtrc(iNO3_)=varid
