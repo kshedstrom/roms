d7004ae3843cb8cab6bd0593a78e44bce780f6e6
kshedstrom <kate@arsc.edu>
2014-02-05 17:01:59 -0900

    src:ticket:618
    git-svn-id: https://www.myroms.org/svn/omlab/branches/arango@1551 f091316a-d328-0410-a40a-876eff57d070
    
    Conflicts:
    
    	ROMS/Nonlinear/Biology/nemuro_inp.h

--- ROMS/Functionals/ana_biology.h
+++ ROMS/Functionals/ana_biology.h
@@ -377,6 +377,8 @@
 !  Nemuro lower trophic level ecosystem model.
 !-----------------------------------------------------------------------
 !
+      cff1=20.0_r8/3.0_r8
+      cff2= 2.0_r8/3.0_r8
       DO k=1,N(ng)
         DO j=JstrT,JendT
           DO i=IstrT,IendT
