c8b1d0df9b15a2fc09bcbe8ab5c8ad8d9242e80d
Kate Hedstrom <kshedstrom@alaska.edu>
2014-11-18 15:26:56 -0900

    New Arctic floats

--- Apps/Arctic/arctic.h
+++ Apps/Arctic/arctic.h
@@ -150,9 +150,9 @@
 #  undef ANA_SRFLUX
 #  undef ALBEDO_CLOUD
 #  define ALBEDO_CURVE  /* for water */
-#  define ICE_ALB_EC92  /* for ice */
-#  undef ALBEDO_CSIM   /* for ice */
-#  undef ALBEDO_FILE  /* for both */
+#  undef ICE_ALB_EC92   /* for ice */
+#  define ALBEDO_CSIM   /* for ice */
+#  undef ALBEDO_FILE    /* for both */
 #  undef LONGWAVE
 # endif
 #endif
