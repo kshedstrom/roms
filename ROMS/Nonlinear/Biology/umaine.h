206b7d5e58083732049b37d2b89db3265e426cc6
Kate Hedstrom <kshedstrom@alaska.edu>
2016-04-08 19:29:47 -0800

    Added first draft Glacier Bay

--- ROMS/Nonlinear/Biology/umaine.h
+++ ROMS/Nonlinear/Biology/umaine.h
@@ -566,7 +566,7 @@
           indx=idbio(ibio)
           DO k=1,N(ng)
             DO i=Istr,Iend
-              Bio_bak(i,k,indx)=MAX(t(i,j,k,nstp,indx),0.0001_r8)
+              Bio_bak(i,k,indx)=MAX(t(i,j,k,nstp,indx),Minval)
               Bio(i,k,indx)=Bio_bak(i,k,indx)
             END DO
           END DO
@@ -1089,24 +1089,24 @@
               cffFeS2_R=dtdays*(FCratioE-FCratioS2)/T_fe(ng)
               cffFeS2_R=Bio(i,k,iS2_C)*cffFeS2_R                        !used biomass in C - no need for redfield conversion (106.0_r8/16.0_r8)
               IF (cffFeS2_R.ge.0.0_r8) THEN
-                cffFeS2_R=cffFeS2_R/MAX(MinVal,Bio(i,k,iFeD_))
+                cffFeS2_R=cffFeS2_R/MAX(Minval,Bio(i,k,iFeD_))
               ELSE
-                cffFeS2_R=-cffFeS2_R/MAX(MinVal,Bio(i,k,iS2_Fe))
+                cffFeS2_R=-cffFeS2_R/MAX(Minval,Bio(i,k,iS2_Fe))
               END IF
 
 
 !For S3
 ! Iron uptake proportional to growth
-              FNratioS3=Bio(i,k,iS3_Fe)/MAX(MinVal,Bio(i,k,iS3_N))
-              cffFeS3_G=n_pps3*FNratioS3/MAX(MinVal,Bio(i,k,iFeD_))
+              FNratioS3=Bio(i,k,iS3_Fe)/MAX(Minval,Bio(i,k,iS3_N))
+              cffFeS3_G=n_pps3*FNratioS3/MAX(Minval,Bio(i,k,iFeD_))
 
 ! Iron uptake to reach appropriate Fe:C ratio
               cffFeS3_R=dtdays*(FCratioE-FCratioS3)/T_fe(ng)
               cffFeS3_R=Bio(i,k,iS3_C)*cffFeS3_R                         !used biomass in C - no need for redfield conversion (106.0_r8/16.0_r8)
               IF (cffFeS3_R.ge.0.0_r8) THEN
-                cffFeS3_R=cffFeS3_R/MAX(MinVal,Bio(i,k,iFeD_))
+                cffFeS3_R=cffFeS3_R/MAX(Minval,Bio(i,k,iFeD_))
               ELSE
-                cffFeS3_R=-cffFeS3_R/MAX(MinVal,Bio(i,k,iS3_Fe))
+                cffFeS3_R=-cffFeS3_R/MAX(Minval,Bio(i,k,iS3_Fe))
               END IF
 
 #endif
@@ -1889,7 +1889,7 @@
             END DO
             DO k=1,N(ng)
               Bio(i,k,indx)=Bio(i,k,indx)-dtdays*sinkindx(k)
-              Bio(i,k,indx)=max(Bio(i,k,indx),0.00001_r8)
+              Bio(i,k,indx)=max(Bio(i,k,indx),Minval)
             END DO
           END DO
         END DO SINK_LOOP
@@ -2100,7 +2100,7 @@
               t(i,j,k,nnew,indx)=MAX(t(i,j,k,nnew,indx)+                &
      &                               (Bio(i,k,indx)-Bio_bak(i,k,indx))* &
      &                               Hz(i,j,k),                         &
-     &                               0.0001_r8)
+     &                               Minval)
 !#ifdef TS_MPDATA
 !              t(i,j,k,3,indx)=t(i,j,k,nnew,indx)*Hz_inv(i,k)
 !#endif
