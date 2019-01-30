741ad632829939034ea0f03798f3e98d46c30acb
arango <arango@f091316a-d328-0410-a40a-876eff57d070>
2019-01-30 17:32:46 +0000

    src:ticket:797
    git-svn-id: https://www.myroms.org/svn/src/trunk@942 f091316a-d328-0410-a40a-876eff57d070

--- Compilers/Linux-gfortran.mk
+++ Compilers/Linux-gfortran.mk
@@ -17,9 +17,13 @@
 # CFLAGS         Flags to the C compiler
 # CXX            Name of the C++ compiler
 # CXXFLAGS       Flags to the C++ compiler
+# HDF5_INCDIR    HDF5 include directory
+# HDF5_LIBDIR    HDF5 library directory
+# HDF5_LIBS      HDF5 library switches
 # NF_CONFIG      NetCDF Fortran configuration script
 # NETCDF_INCDIR  NetCDF include directory
 # NETCDF_LIBDIR  NetCDF library directory
+# NETCDF_LIBS    NetCDF library switches
 # LD             Program to load the objects into an executable
 # LDFLAGS        Flags to the loader
 # RANLIB         Name of ranlib command
@@ -90,6 +94,45 @@ ifdef CICE_APPLICATION
 endif
 
 #--------------------------------------------------------------------------
+# Coupled models.  Notice Linux needs the libraries repeated for
+# dependencies for some of the coupled components.
+#--------------------------------------------------------------------------
+
+ifdef USE_COAMPS
+             LIBS += $(COAMPS_LIB_DIR)/coamps_driver.a
+             LIBS += $(COAMPS_LIB_DIR)/libaa.a
+             LIBS += $(COAMPS_LIB_DIR)/libam.a
+             LIBS += $(COAMPS_LIB_DIR)/libashare.a
+             LIBS += $(COAMPS_LIB_DIR)/libcoamps.a
+             LIBS += $(COAMPS_LIB_DIR)/libfnoc.a
+             LIBS += $(COAMPS_LIB_DIR)/libaa.a
+             LIBS += $(COAMPS_LIB_DIR)/libam.a
+             LIBS += $(COAMPS_LIB_DIR)/libashare.a
+             LIBS += $(COAMPS_LIB_DIR)/libcoamps.a
+             LIBS += $(COAMPS_LIB_DIR)/libfnoc.a
+             LIBS += $(COAMPS_LIB_DIR)/libfishpak.a
+             LIBS += $(COAMPS_LIB_DIR)/libtracer.a
+endif
+
+ifdef USE_WRF
+             LIBS += $(WRF_LIB_DIR)/module_wrf_top.o
+             LIBS += $(WRF_LIB_DIR)/libwrflib.a
+             LIBS += $(WRF_LIB_DIR)/libfftpack.a
+             LIBS += $(WRF_LIB_DIR)/libio_grib1.a
+             LIBS += $(WRF_LIB_DIR)/libio_grib_share.a
+             LIBS += $(WRF_LIB_DIR)/libwrfio_int.a
+             LIBS += $(WRF_LIB_DIR)/libesmf_time.a
+             LIBS += $(WRF_LIB_DIR)/librsl_lite.a
+             LIBS += $(WRF_LIB_DIR)/module_internal_header_util.o
+             LIBS += $(WRF_LIB_DIR)/pack_utils.o
+             LIBS += $(WRF_LIB_DIR)/libwrfio_nf.a
+endif
+
+ifdef CICE_APPLICATION
+            SLIBS := $(LIBS) $(SLIBS)
+endif
+
+#--------------------------------------------------------------------------
 # Library locations, can be overridden by environment variables.
 #--------------------------------------------------------------------------
 
@@ -103,14 +146,16 @@ ifdef USE_NETCDF4
 else
     NETCDF_INCDIR ?= /opt/gfortransoft/serial/netcdf3/include
     NETCDF_LIBDIR ?= /opt/gfortransoft/serial/netcdf3/lib
-             LIBS += -L$(NETCDF_LIBDIR) -lnetcdf
+      NETCDF_LIBS ?= -lnetcdf
+             LIBS += -L$(NETCDF_LIBDIR) $(NETCDF_LIBS)
            INCDIR += $(NETCDF_INCDIR) $(INCDIR)
 endif
 
 ifdef USE_HDF5
       HDF5_INCDIR ?= /opt/gfortransoft/serial/hdf5/include
       HDF5_LIBDIR ?= /opt/gfortransoft/serial/hdf5/lib
-             LIBS += -L$(HDF5_LIBDIR) -lhdf5_fortran -lhdf5hl_fortran -lhdf5 -lz
+        HDF5_LIBS ?= -lhdf5_fortran -lhdf5hl_fortran -lhdf5 -lz
+             LIBS += -L$(HDF5_LIBDIR) $(HDF5_LIBS)
            INCDIR += $(HDF5_INCDIR)
 endif
 
